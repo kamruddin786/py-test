46251e8d4e70c4568490007eca18b67d2acd5948
kamruddin786 <kamruddin.mansoori@gmail.com>
2024-03-15 17:50:20 +0530

    Final commit.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # py-test
-python test code
+python test 
+
+install dependencies using below command
+tabula also requires - java 
+pip install tabula-py pandas openpyxl
+
+  git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
---
+++ test2.py
@@ -0,0 +1,12 @@
+import tabula
+import pandas as pd
+
+# Path to your file
+file = "Encashment details _Final.pdf"
+
+# Read the PDF as DataFrame
+tables = tabula.read_pdf(file, pages='all', multiple_tables=True)
+
+# Loop to save each table into separate CSV
+for i, table in enumerate(tables, start=1):
+    table.to_excel(f'encash/Encashment_details_{i}.xlsx', index=False)
